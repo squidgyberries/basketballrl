64091e563dcabf69be483d9025c958e4c92ed76b
squidgyberries <squidge@tuta.io>
2025-07-17 14:25:37 -0400

    all the stuffs

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-__credits__ = ["Andrea PIERRÉ"]
-
 import math
 import os
 from typing import TYPE_CHECKING
@@ -16,6 +14,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
 
 
 try:
@@ -37,13 +36,13 @@ except ImportError as e:
 if TYPE_CHECKING:
     import pygame
 
-FPS = 50
+FPS = 60
 SCALE = 30.0  # affects how fast-paced the game is, forces should be adjusted as well
 
-MOTORS_TORQUE = 80
-SPEED_SHOULDER = 5
-SPEED_ELBOW = 5
-SPEED_WRIST = 5
+MOTORS_TORQUE = 100
+SPEED_SHOULDER = 7
+SPEED_ELBOW = 7
+SPEED_WRIST = 7
 
 INITIAL_RANDOM = 5
 
@@ -52,8 +51,11 @@ ARM_W = 4 / SCALE
 HAND_L = 25 / SCALE
 HAND_W = 4 / SCALE
 
-VIEWPORT_W = 600
-VIEWPORT_H = 400
+HOOP_MIN_DIST = 8
+HOOP_MAX_DIST = 9
+
+VIEWPORT_W = 800
+VIEWPORT_H = 600
 
 TERRAIN_STEP = 14 / SCALE
 TERRAIN_LENGTH = 200  # in steps
@@ -92,7 +94,7 @@ FOREARM_FD = fixtureDef(
 HAND_FD = fixtureDef(
     shape=polygonShape(box=(HAND_L / 2, HAND_W / 2)),
     density=1.0,
-    friction=1.0,
+    friction=5.0,
     restitution=0.0,
     categoryBits=0x0020,
     maskBits=0x001,
@@ -105,36 +107,36 @@ class ContactDetector(contactListener):
         self.env = env
 
     def BeginContact(self, contact):
-        if (
-            self.env.hull == contact.fixtureA.body
-            or self.env.hull == contact.fixtureB.body
-        ):
-            # self.env.game_over = True
-            pass
-        # for arm in [self.env.arms[1]]:
-        #     if arm in [contact.fixtureA.body, contact.fixtureB.body]:
-        #         arm.ground_contact = True
+        bodies = [contact.fixtureA.body, contact.fixtureB.body]
+        if self.env.ball in bodies:
+            for body in bodies:
+                if hasattr(body, 'userData') and body.userData == 'hoop_sensor':
+                    self.env.ball_in_hoop = True
 
     def EndContact(self, contact):
-        # for arm in [self.env.arms[1]]:
-        #     if arm in [contact.fixtureA.body, contact.fixtureB.body]:
-        #         arm.ground_contact = False
-        pass
+        bodies = [contact.fixtureA.body, contact.fixtureB.body]
+        if self.env.ball in bodies:
+            for body in bodies:
+                if hasattr(body, 'userData') and body.userData == 'hoop_sensor':
+                    self.env.ball_in_hoop = False
+                    # Set the flag only when ball exits the hoop
+                    if self.env.ball.linearVelocity.y < 0 and self.env.ball.position[1] > 8:
+                        self.env.shot_made = True
 
 class BasketballArm(gym.Env, EzPickle):
 
     def _add_hoop(self, position=(12,8)):
         x, y= position 
         #backboard
-        backboard = self.world.CreateStaticBody(
+        self.backboard = self.world.CreateStaticBody(
             position=(x+0.5, y+1),
             shapes=polygonShape(box=(0.2, 1.5))
         )
-        backboard.color1=(150,150,150)
-        backboard.color2=(100,100,100)
-        self.drawlist.append(backboard)
+        self.backboard.color1=(150,150,150)
+        self.backboard.color2=(100,100,100)
+        self.drawlist.append(self.backboard)
         #rim
-        rim = self.world.CreateStaticBody(
+        self.rim = self.world.CreateStaticBody(
             position = (x,y),
             fixtures=fixtureDef(
                 shape=polygonShape(box=(1.1, 0.1)),
@@ -142,20 +144,21 @@ class BasketballArm(gym.Env, EzPickle):
                 )
         )
 
-        rim.color1 = (255,0,0)
-        rim.color2 = (200,0,0)
+        self.rim.color1 = (255,0,0)
+        self.rim.color2 = (200,0,0)
+        self.rim.userData = 'hoop_sensor'
         
-        self.drawlist.append(rim)
+        self.drawlist.append(self.rim)
 
         #supports
 
-        support = self.world.CreateStaticBody(
+        self.support = self.world.CreateStaticBody(
             position=(x-1.15 , y + 0.15),
             shapes=polygonShape(box=(0.05, 0.2))
         )
-        support.color1 = (255, 0, 0)
-        support.color2 = (200, 0, 0)
-        self.drawlist.append(support)
+        self.support.color1 = (255, 0, 0)
+        self.support.color2 = (200, 0, 0)
+        self.drawlist.append(self.support)
 
     def _add_ball(self, position=(10, 10), radius=0.5):
         ball_fixture = fixtureDef(
@@ -268,26 +271,54 @@ class BasketballArm(gym.Env, EzPickle):
         # 5 x the rated speed due to impulses from ground contact etc.
         low = np.array(
             [
-                -np.inf,
-                -np.inf,
-                -5.0,
+                -20,
+                -20,
+                -20,
+                -20,
+                # -20,
+                # -20,
+                -20,
+                -20,
+                # -20,
+                # -20,
                 -math.pi,
                 -5.0,
+                -20,
+                -20,
                 -math.pi,
                 -5.0,
+                -20,
+                -20,
                 -math.pi,
+                -5.0,
+                -20,
+                -20,
             ]
         ).astype(np.float32)
         high = np.array(
             [
-                np.inf,
-                np.inf,
-                5.0,
+                20,
+                20,
+                20,
+                20,
+                # 20,
+                # 20,
+                20,
+                20,
+                # 20,
+                # 20,
                 math.pi,
                 5.0,
+                20,
+                20,
                 math.pi,
                 5.0,
+                20,
+                20,
                 math.pi,
+                5.0,
+                20,
+                20,
             ]
         ).astype(np.float32)
         self.action_space = spaces.Box(
@@ -302,6 +333,10 @@ class BasketballArm(gym.Env, EzPickle):
 
         self.max_x = 0
         self.max_y = 0
+        self.prev_action = np.zeros(3)
+        self.shot_made = False
+        self.ball_in_hoop = False
+        self.closest_distance = np.inf
 
     def _destroy(self):
         if not self.terrain:
@@ -318,6 +353,12 @@ class BasketballArm(gym.Env, EzPickle):
         self.joints = []
         self.world.DestroyBody(self.ball)
         self.ball = None
+        self.world.DestroyBody(self.backboard)
+        self.backboard = None
+        self.world.DestroyBody(self.rim)
+        self.rim = None
+        self.world.DestroyBody(self.support)
+        self.support = None
         
 
     def _generate_terrain(self):
@@ -372,11 +413,11 @@ class BasketballArm(gym.Env, EzPickle):
         self._generate_terrain()
         print(self.terrain)
 
-        init_x = TERRAIN_STEP * TERRAIN_STARTPAD / 2
-        init_y = TERRAIN_HEIGHT + 2 * ARM_L
+        self.init_x = TERRAIN_STEP * TERRAIN_STARTPAD
+        self.init_y = TERRAIN_HEIGHT + 2 * ARM_L
         
         self.hull = self.world.CreateStaticBody(
-            position=(init_x, init_y), fixtures=HULL_FD,
+            position=(self.init_x, self.init_y), fixtures=HULL_FD,
         )
         self.hull.color1 = (127, 51, 229)
         self.hull.color2 = (76, 76, 127)
@@ -384,7 +425,7 @@ class BasketballArm(gym.Env, EzPickle):
         self.arms: list[Box2D.b2Body] = []
         self.joints: list[Box2D.b2RevoluteJoint] = []
         upper_arm = self.world.CreateDynamicBody(
-            position=(init_x + ARM_L / 2, init_y),
+            position=(self.init_x + ARM_L / 2, self.init_y),
             angle=0,
             fixtures=UPPER_ARM_FD,
         )
@@ -406,7 +447,7 @@ class BasketballArm(gym.Env, EzPickle):
         self.joints.append(self.world.CreateJoint(rjd))
 
         forearm = self.world.CreateDynamicBody(
-            position=(init_x + ARM_L, init_y + ARM_L / 2),
+            position=(self.init_x + ARM_L, self.init_y + ARM_L / 2),
             angle=0,
             fixtures=FOREARM_FD,
         )
@@ -428,7 +469,7 @@ class BasketballArm(gym.Env, EzPickle):
         self.joints.append(self.world.CreateJoint(rjd))
 
         hand = self.world.CreateDynamicBody(
-            position=(init_x + ARM_L - HAND_L / 2, init_y + ARM_L),
+            position=(self.init_x + ARM_L - HAND_L / 2, self.init_y + ARM_L),
             angle=0,
             fixtures=HAND_FD,
         )
@@ -443,23 +484,34 @@ class BasketballArm(gym.Env, EzPickle):
             enableLimit=True,
             maxMotorTorque=MOTORS_TORQUE,
             motorSpeed=0,
-            lowerAngle=-np.pi / 2,
-            upperAngle=np.pi / 3,
+            lowerAngle=-np.pi,
+            upperAngle=0,
         )
         self.arms.append(hand)
         self.joints.append(self.world.CreateJoint(rjd))
 
         self.drawlist = self.terrain + self.arms + [self.hull]
 
-        self._add_ball(position=(init_x + ARM_L - HAND_L / 2 + 0.5, init_y + ARM_L + 0.5), radius=0.5)
-        self._add_hoop()
+        initial_ball_position = (self.init_x + ARM_L - HAND_L / 2, self.init_y + ARM_L + 0.5)
+        self._add_ball(position=initial_ball_position, radius=0.5)
+        self.hoop_position = (self.init_x + np.random.uniform(HOOP_MIN_DIST, HOOP_MAX_DIST), self.init_y + 4)
+        # self.hoop_position = (self.init_x + 9, self.init_y + 5)
+        self._add_hoop(position=self.hoop_position)
+        self.closest_distance = math.sqrt((initial_ball_position[0] - self.hoop_position[0]) ** 2 + (initial_ball_position[1] - self.hoop_position[1]) ** 2)
+        self.target_y = self.init_y + 7
+        self.closest_y_distance = self.target_y - initial_ball_position[1]
+        self.target_x = self.hoop_position[0]
+        self.closest_x_distance = self.target_x - initial_ball_position[0]
 
         self.max_x = 0
         self.max_y = 0
+        self.prev_action = np.zeros(3)
+        self.shot_made = False
+        self.ball_in_hoop = False
 
         if self.render_mode == "human":
             self.render()
-        return self.step(np.array([0, 0, 0, 0]))[0], {}
+        return self.step(np.array([0, 0, 0]))[0], {}
 
     def step(self, action: np.ndarray):
         assert self.hull is not None
@@ -487,28 +539,35 @@ class BasketballArm(gym.Env, EzPickle):
         self.world.Step(1.0 / FPS, 6 * 30, 2 * 30)
 
         pos = self.hull.position
-        vel = self.hull.linearVelocity
+        # vel = self.hull.linearVelocity
 
         state = [
-            # self.hull.angle,  # Normal angles up to 0.5 here, but sure more is possible.
-            # 2.0 * self.hull.angularVelocity / FPS,
-            # 0.3 * vel.x * (VIEWPORT_W / SCALE) / FPS,  # Normalized to get -1..1 range
-            # 0.3 * vel.y * (VIEWPORT_H / SCALE) / FPS,
+            self.hoop_position[0],
+            self.hoop_position[1],
             self.ball.position[0],
             self.ball.position[1],
+            # self.ball.position[0] - self.hoop_position[0],
+            # self.ball.position[1] - self.hoop_position[1],
+            self.ball.linearVelocity[0],
+            self.ball.linearVelocity[1],
+            # self.arms[2].position[0] - self.ball.position[0],
+            # self.arms[2].position[1] - self.ball.position[1],
             self.joints[0].angle,
-            # This will give 1.1 on high up, but it's still OK (and there should be spikes on hitting the ground, that's normal too)
             self.joints[0].speed / SPEED_SHOULDER,
+            self.arms[0].position[0],
+            self.arms[0].position[1],
             self.joints[1].angle,
             self.joints[1].speed / SPEED_ELBOW,
+            self.arms[1].position[0],
+            self.arms[1].position[1],
             self.joints[2].angle,
             self.joints[2].speed / SPEED_WRIST,
-            # 1.0 if self.arms[1].ground_contact else 0.0,
-            # 0.0,
+            self.arms[2].position[0],
+            self.arms[2].position[1],
         ]
         # assert len(state) == 24
         # print(len(state))
-        assert len(state) == 8
+        assert len(state) == 18
 
         self.scroll = pos.x - VIEWPORT_W / SCALE / 5
 
@@ -525,17 +584,42 @@ class BasketballArm(gym.Env, EzPickle):
         self.prev_shaping = shaping
 
         ball_x, ball_y = self.ball.position[0], self.ball.position[1]
+        reward = 0
 
-        self.max_x = max(self.max_x, ball_x)
-        self.max_y = max(self.max_y, ball_y)
+        distance = math.sqrt((ball_x - self.hoop_position[0]) ** 2 + (ball_y - self.hoop_position[1]) ** 2)
+        if distance < self.closest_distance:
+            reward += 1.0 * (self.closest_distance - distance)
+            self.closest_distance = distance
+        
+        y_distance = self.target_y - ball_y
+        if y_distance < self.closest_y_distance and y_distance >= 0:
+            reward += 0.8 * (self.closest_y_distance - y_distance)
+            self.closest_y_distance = y_distance
+        
+        x_distance = self.target_x - ball_x
+        if x_distance < self.closest_x_distance and x_distance >= 0:
+            reward += 0.8 * (self.closest_x_distance - x_distance)
+            self.closest_x_distance = x_distance
 
-        reward = self.max_x * 0.5 + self.max_y  # prioritize vertical distance
+        # if ball_x > self.max_x:
+        #     reward += ball_x - self.max_x
+        #     self.max_x = ball_x
+        # if ball_y > self.max_y:
+        #     reward += ball_y - self.max_y
+        #     self.max_y = ball_y
 
-        for a in action:
-            reward -= 0.0003 * MOTORS_TORQUE * np.clip(np.abs(a), 0, 1)
+        action_change_penalty = np.sum((action - self.prev_action) ** 2)
+        reward -= 0.1 * action_change_penalty  # scale to balance
+        # for a in action:
+            # reward -= 0.003 * MOTORS_TORQUE * np.clip(np.abs(a), 0, 1)
+        
+        if self.shot_made:
+            reward += 50
+            self.shot_made = False
 
         terminated = False
         if self.ball.position[1] <= TERRAIN_HEIGHT + 0.6:
+            # reward += 100 * (self.max_x * 0.5 + self.max_y)
             terminated = True
 
         if self.render_mode == "human":
@@ -652,46 +736,6 @@ class BasketballArm(gym.Env, EzPickle):
             self.isopen = False
 
 
-class BasketballArmHeuristics:
-    def __init__(self):
-        self.phase = 0
-        self.timer = 0
-        self.action = np.zeros(3)
-
-        # Define joint angle targets for a basic shooting sequence
-        self.phases = [
-            {"shoulder": -0.5, "elbow": 0.8, "wrist": -0.2, "duration": 30},  # Wind-up
-            {"shoulder": 0.3, "elbow": -0.2, "wrist": 1.0, "duration": 10},  # Throw
-            {"shoulder": 0.0, "elbow": 0.0, "wrist": 0.0, "duration": 20},   # Follow-through / Reset
-        ]
-
-    def step_heuristic(self, state):
-        target = self.phases[self.phase]
-        shoulder_angle = state[2]
-        shoulder_speed = state[3]
-        elbow_angle = state[4]
-        elbow_speed = state[5]
-        wrist_angle = state[6]
-        wrist_speed = state[7]
-
-        # PID-like control: target - current - some damping
-        shoulder_todo = 5.0 * (target["shoulder"] - shoulder_angle) - 0.2 * shoulder_speed
-        elbow_todo = 5.0 * (target["elbow"] - elbow_angle) - 0.2 * elbow_speed
-        wrist_todo = 5.0 * (target["wrist"] - wrist_angle) - 0.2 * wrist_speed
-
-        self.action[0] = np.clip(shoulder_todo, -1.0, 1.0)
-        self.action[1] = np.clip(elbow_todo, -1.0, 1.0)
-        self.action[2] = np.clip(wrist_todo, -1.0, 1.0)
-
-        self.timer += 1
-        if self.timer >= target["duration"]:
-            self.phase = (self.phase + 1) % len(self.phases)
-            self.timer = 0
-
-        # return self.action
-        return np.array([0.5, 0.5, -1.0])
-
-
 def make_env(record_video=False, video_folder="videos", video_length=1000):
     def _init():
         env = BasketballArm(render_mode='rgb_array')
@@ -705,7 +749,7 @@ def make_env(record_video=False, video_folder="videos", video_length=1000):
         env = VecVideoRecorder(
             env,
             video_folder=video_folder,
-            record_video_trigger=lambda x: x % 10000 == 0,
+            record_video_trigger=lambda x: x % 50000 == 0,
             video_length=video_length,
             name_prefix="basketball_arm_episode",
         )
@@ -713,15 +757,18 @@ def make_env(record_video=False, video_folder="videos", video_length=1000):
     return env
 
 
-def train_model(total_timesteps=200_000, log_dir="logs"):
+def train_model(total_timesteps=501_000, log_dir="logs"):
     os.makedirs(log_dir, exist_ok=True)
     os.makedirs("videos", exist_ok=True)
 
     env = make_env(record_video=True)
+    eval_env = make_env(record_video=False)
+    eval_callback = EvalCallback(eval_env,
+                             log_path=log_dir, eval_freq=50000,
+                             deterministic=True)
 
-    # model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=total_timesteps)
+    model = PPO("MlpPolicy", env, verbose=0, tensorboard_log=log_dir)
+    model.learn(total_timesteps=total_timesteps, callback=eval_callback)
     model.save("ppo_basketball_arm")
 
     return model
@@ -750,35 +797,3 @@ if __name__ == "__main__":
 
     # Evaluate and visualize
     evaluate_and_render()
-
-# if __name__ == "__main__":
-#     env = BasketballArm(render_mode="human")
-#     env.reset()
-
-    
-#     steps = 0
-#     total_reward = 0
-#     a = np.array([0.0, 0.0, 0.0])
-#     # Heurisic: suboptimal, have no notion of balance.
-#     heuristics = BasketballArmHeuristics()
-#     while True:
-#         s, r, terminated, truncated, info = env.step(a)
-#         total_reward += r
-#         if steps % 20 == 0 or terminated or truncated:
-#             print("\naction " + str([f"{x:+0.2f}" for x in a]))
-#             print(f"step {steps} total_reward {total_reward:+0.2f}")
-#             print("hull " + str([f"{x:+0.2f}" for x in s[0:4]]))
-#             print("leg0 " + str([f"{x:+0.2f}" for x in s[4:9]]))
-#             print("leg1 " + str([f"{x:+0.2f}" for x in s[9:14]]))
-#         steps += 1
-
-#         a = heuristics.step_heuristic(s)
-
-#         if steps > 300:
-#             env.reset()
-#             steps = 0
-#             total_reward = 0
-#             a = np.array([0.0, 0.0, 0.0])
-
-#         if terminated or truncated:
-#             break
